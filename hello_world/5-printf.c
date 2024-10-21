f6fc159ffd7070ea1b198b6a6ade995cae19ed10
korneel <korneel.loy@gmail.com>
2024-10-21 12:24:47 +0000

    C ex 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+/**
+ * main - the main funnction
+ * @void the main function doesn't take any parameters
+ *
+ * Return: the main function returns 0 if no erreors occured
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
