fad03c2fd11bb4ffd770bf3b357762734312161b
korneel <korneel.loy@gmail.com>
2024-10-21 12:01:01 +0000

    C ex 4 / with comments

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - main function
+ * @void: no parameters for the main function
+ *
+ * Return: return 0 if no errors
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
