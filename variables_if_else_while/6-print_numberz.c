e854f0273398370cfb451b34ad68c5ea55a269c2
korneel <korneel.loy@gmail.com>
2024-10-22 11:27:54 +0000

    C boucle ex 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 is no errors
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar('0'+i);
+	putchar(10);
+	return (0);
+}
