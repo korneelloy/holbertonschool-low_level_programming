4b61d01c7578e3cbd929496b683597d04e577a22
korneel <korneel.loy@gmail.com>
2024-10-23 13:57:59 +0000

    C functions ex 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n : number give
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int dernier;
+
+	dernier = n % 10;
+	if (dernier < 0)
+	{
+		dernier = dernier * -1;
+	}
+	_putchar(dernier);
+	return (dernier);
+}
