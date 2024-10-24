bb82a6c50ca2584afa7435c2f0038d059ebe1b8a
korneel <korneel.loy@gmail.com>
2024-10-24 13:06:35 +0000

    C functions ex 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - adding 2 integers and return result
+ * @num1 : first number
+ * @num2 : second number
+ *
+ * Return: result of sum
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
