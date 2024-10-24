ce8e77844eb8a131f6743077e2c2e5aa9a4d4954
korneel <korneel.loy@gmail.com>
2024-10-24 14:36:12 +0000

    C functions ex 11 en cours

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * print_to_98 - will print all integers between given nb and 98
+ * @n number from where printing will start
+ *
+ * Reurn: no return for this function
+ */
+
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		printf("%d", n);
+	}
+	else if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d", n);
+			n++;
+		}
+	}
+	else
+	{
+		while (n > 98)
+		{
+			printf("%d", n);
+			n--;
+		}
+	}
+}
