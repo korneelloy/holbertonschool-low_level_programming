16df24562a9d8681874aa31fb57819e6b6ef7636
korneel <korneel.loy@gmail.com>
2024-10-25 07:09:40 +0000

    C functions ex 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,15 +8,11 @@
 
 void print_to_98(int n)
 {
-	if (n == 98)
-	{
-		printf("%d", n);
-	}
-	else if (n < 98)
+	if (n < 98)
 	{
 		while (n < 98)
 		{
-			printf("%d", n);
+			printf("%d, ", n);
 			n++;
 		}
 	}
@@ -24,8 +20,9 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			printf("%d", n);
+			printf("%d, ", n);
 			n--;
 		}
 	}
+	printf("98\n");
 }
