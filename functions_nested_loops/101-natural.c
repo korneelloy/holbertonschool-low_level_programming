e4340754129bff4df7f16b05ec2bb4d03afed931
korneel <korneel.loy@gmail.com>
2024-10-25 13:15:00 +0000

    C functions ex 13 correction bug

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -13,7 +13,6 @@ int main(void)
 	return (0);
 }
 
-
 /**
  * print_sum_mult_3_5_below_1024 - prints sum of the mult. of 3 & 5 below 1024
  *
@@ -22,23 +21,19 @@ int main(void)
 
 void print_sum_mult_3_5_below_1024(void)
 {
-	int sum_3 = 0;
-	int x = 0;
 	int i;
-	int sum_5 = 0;
-	int y = 0;
-	int j;
+	int sum = 0;
 
-	for (i = 1; x < 1024; i++)
+	for (i = 3; i < 1024; i++)
 	{
-		sum_3 += i * 3;
-		x = i * 3;
+		if ((i % 3) == 0)
+		{
+			sum += i;
+		}
+		else if ((i % 5) == 0)
+		{
+			sum += i;
+		}
 	}
-
-	for (j = 1; y < 1024; j++)
-	{
-		sum_5 += j * 5;
-		y = j * 5;
-	}
-	printf("%d\n", sum_3 + sum_5);
+	printf("%d\n", sum);
 }
