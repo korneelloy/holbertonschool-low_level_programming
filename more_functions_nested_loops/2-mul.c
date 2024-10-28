4f8478042f89a427638ded3894602281443bdde4
korneel <korneel.loy@gmail.com>
2024-10-28 09:50:49 +0000

    More functions and nested loops ex 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiplies 2 numbers*
+ * @a: fi=rst number
+ * @b: second number
+ *
+ * Return: the result oif the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 
 int _putchar(char);
-int _isupper(int c);
-int _isdigit(int c);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 
 #endif
