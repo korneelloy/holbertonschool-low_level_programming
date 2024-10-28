074186061e02a7328b278e20cc9ebbf2a82a7cbe
korneel <korneel.loy@gmail.com>
2024-10-28 10:00:45 +0000

    C more functions and nested loops ex 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - printing numbers for 0 to 1
+ *
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+	_putchar('\n');
+}
