e0b2449cef9a3628332f4c85a66c948f322eed31
korneel <korneel.loy@gmail.com>
2024-10-28 11:14:17 +0000

    C more functions and nested loops ex 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal of n lines for \ character
+ * @n: number of lines
+ *
+ * Return: no return
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		int j;
+
+		for (j = 0; j < i; j++)
+		{
+			_putchar(32);
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
