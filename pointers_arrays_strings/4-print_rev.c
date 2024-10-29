c5e13f190e40e99e92467107a4f8669bf4350b06
korneel <korneel.loy@gmail.com>
2024-10-29 12:36:42 +0000

    pointeur and arrayus ex 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse + newline to stdout
+ * @s: pointer to a character / first character of a string
+ *
+ * Return: no return
+ */
+
+void print_rev(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+		j = i;
+	for (; j >= 0; j--)
+		_putchar(s[j]);
+	_putchar('\n');
+}
