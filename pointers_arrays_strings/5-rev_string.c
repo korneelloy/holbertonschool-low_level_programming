e7158dbdcfd124d5f1f7e12d787822457d88432a
korneel <korneel.loy@gmail.com>
2024-10-29 17:57:55 +0000

    C pointeurs ex 5 en cours

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,17 +7,40 @@
  * Return: no return
  */
 
+int compter_nombre_char(char *s)
+{
+        int i, j;
+
+        for (i = 0; s[i] != '\0'; i++)
+                j = i;
+
+        return (j);
+}
+
 void rev_string(char *s)
 {
-	int i, j;
+	int nombre_char = compter_nombre_char(s);
+	int i;
+	int j = 0;
+	int k;
 
-	for (i = 0; s[i] != '\0'; i++)
-		j = i;
-/**	total = j + 1;*/
+	for (i = nombre_char; i == 0; i --)
+	{
+		j++;
+		s[i] = s[j];
+	}
 
+	for (k = 0; k < nombre_char; k++)
+		putchar
+
+/**
+
+	
 	for (; j > 0; j--)
 	{
 		printf("%c", s[j]);
-		       /**	= s[total-j];*/
+		       = s[total-j];
 	}
+*/
+
 }
