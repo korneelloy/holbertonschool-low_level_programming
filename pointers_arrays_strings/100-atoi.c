30d786dade3822cfeae188e212d26c80bb499aca
korneel <korneel.loy@gmail.com>
2024-10-30 19:47:34 +0000

    C pointers ex 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,45 @@
-#include <stdio.h>
+int pos_or_neg(char *s);
+int get_first(char *s);
+int get_last(char *s);
+
 /**
  * _atoi - convert string to integer
  * @s: pointer to string
  *
  * Return: the converted integer
+ */
+
+int _atoi(char *s)
+{
+	int sign = pos_or_neg(s);
+	int first = get_first(s);
+	int last = get_last(s);
+	int times = last - first;
+	double dig = 1;
+	int i = -1;
+	int j;
+	double reponse = 0;
+
+	for (j = 0; j < times; j++)
+		dig = dig * 10;
+	for (i = first; i <= last; i++)
+	{
+		reponse += (s[i] - 48) * dig;
+		dig = dig / 10;
+	}
+	if (sign == -1)
+		reponse = 0 - reponse;
+	if (i == -1)
+		reponse = 0;
+	return (reponse);
+}
+
+
+/**
+ * pos_or_neg - is the string positif or negatif
+ * @s: pointer to a string
+ *
+ * Return: -1 if negatif +1 if positif
  */
 
 int pos_or_neg(char *s)
@@ -13,7 +49,7 @@ int pos_or_neg(char *s)
 	int compteur_pos = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{       
+	{
 		if (s[i] == '-')
 			compteur_neg++;
 		else if (s[i] == '+')
@@ -26,6 +62,13 @@ int pos_or_neg(char *s)
 	else
 		return (1);
 }
+
+/**
+ * get_first - get first number in the string
+ * @s: pointer to a string
+ *
+ * Return: position / index of first number
+ */
 
 int get_first(char *s)
 {
@@ -42,6 +85,13 @@ int get_first(char *s)
 	}
 	return (first);
 }
+
+/**
+ * get_last - get last number in the string
+ * @s: pointer to a string
+ *
+ * Return: position / index of last number
+ */
 
 int get_last(char *s)
 {
@@ -65,92 +115,3 @@ int get_last(char *s)
 	}
 	return (last);
 }
-
-int _atoi(char *s)
-{
-	int sign = pos_or_neg(s);
-	int first = get_first(s);
-	int last = get_last(s);
-	int times = last - first;
-	double dig = 1;
-	int i = -1;
-	int j;
-	double reponse = 0;
-
-	for (j = 0; j < times; j++)
-		dig = dig * 10;
-	
-	for (i = first; i <= last; i++)
-        {
-                reponse += (s[i] - 48) * dig;
-		dig = dig / 10;
-        }
-	printf("apres boucle: %d\n", i);
-        if (sign == -1)
-        {
-                reponse = 0 - reponse;
-        }
-        if (i == -1)
-        {
-                reponse = 0;
-        }
-        return (reponse);
-}
-
-
-/**
-int _atoi(char *s)
-{
-	int i = 0;
-	int j = -1;
-	int compteur_neg = 0;
-	int compteur_pos = 0;
-	int first = -1;
-	int last = -2;
-	int compteur_dig = 1;
-	int reponse = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{	
-		if (s[i] == '-')
-		{
-			compteur_neg++;
-		}
-		if (s[i] == '+')
-		{
-			compteur_pos++;
-		}
-		if (s[i] > 47 && s[i] < 58)
-		{
-			if (first == -1)
-			{
-				first = i;
-			}
-			if (s[i + 1] > 47 && s[i + 1] < 58)
-			{
-				compteur_dig = compteur_dig * 10;
-				continue;
-			}
-			else
-			{
-				last = i;
-				break;
-			}
-		}
-	}
-	for (j = first; j <= last; j++)
-	{
-		reponse += (s[j] - 48) * compteur_dig;
-		compteur_dig = compteur_dig / 10;
-	}
-	if (compteur_neg > compteur_pos)
-	{
-		reponse = 0 - reponse;
-	}
-	if (j == -1)
-	{
-		reponse = 0;
-	}
-	return (reponse);
-}
-*/
