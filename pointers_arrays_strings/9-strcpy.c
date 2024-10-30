3e48427921a83939a185e2d391f41fd4045a96fd
korneel <korneel.loy@gmail.com>
2024-10-30 13:03:08 +0000

    C pointers ex 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+/**
+ * _strcpy - copies string
+ * @dest: pointer to 1st char of destination
+ * @src: pointer to the 1st char of string to be copied
+ *
+ * Return: pointer to destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, str_len = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+		str_len++;
+	}
+	dest[str_len + 1] = '\0';
+	return (dest);
+}
