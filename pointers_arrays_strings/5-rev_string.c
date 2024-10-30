5b72334142d4928bfea3fb0a40ac09bf2050f7b7
korneel <korneel.loy@gmail.com>
2024-10-30 08:50:28 +0000

    C pointers ex 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,46 +1,54 @@
-#include <stdio.h>
 #include "main.h"
+void switching(char *a, char *b);
+int compter_nombre_char(char *s);
+
 /**
  * rev_string - reverses a string
- * @s: pointeur vers charcarter or first char of string
+ * @s: pointeur vers chararter or first char of string
  *
  * Return: no return
  */
-
-int compter_nombre_char(char *s)
-{
-        int i, j;
-
-        for (i = 0; s[i] != '\0'; i++)
-                j = i;
-
-        return (j);
-}
 
 void rev_string(char *s)
 {
 	int nombre_char = compter_nombre_char(s);
 	int i;
-	int j = 0;
-	int k;
 
-	for (i = nombre_char; i == 0; i --)
+	for (i = 0; i < (nombre_char / 2) ; i++)
 	{
-		j++;
-		s[i] = s[j];
+		switching(&s[i], &s[nombre_char - i]);
 	}
-
-	for (k = 0; k < nombre_char; k++)
-		putchar
+}
 
 /**
+ * compter_nombre_char - count number of char in a string
+ * @s: pointeur vers character or first char of string
+ *
+ * Return: number of characters in the string
+ */
 
-	
-	for (; j > 0; j--)
-	{
-		printf("%c", s[j]);
-		       = s[total-j];
-	}
-*/
+int compter_nombre_char(char *s)
+{
+	int i, j;
 
+	for (i = 0; s[i] != '\0'; i++)
+	j = i;
+	return (j);
+}
+
+/**
+ * switching - switch both parameters in array
+ * @a: character in the beginning of the string
+ * @b: character at the mirror place at the end of the string
+ *
+ * Return: no return
+ */
+
+void switching(char *a, char *b)
+{
+	char x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
 }
