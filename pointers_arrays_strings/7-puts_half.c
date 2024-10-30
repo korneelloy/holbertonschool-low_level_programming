3b88fdf8b9c976bace76201c89b75939badeb039
korneel <korneel.loy@gmail.com>
2024-10-30 10:53:16 +0000

    C pointeurs ex 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a string
+ * @str: the string
+ *
+ * Return: no return
+ */
+
+void puts_half(char *str)
+{
+	int length_of_the_string = 0;
+	int i, n, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+		length_of_the_string++;
+	if (length_of_the_string % 2 == 1)
+		n = (length_of_the_string - 1) / 2;
+	else
+		n = length_of_the_string / 2;
+	for (j = n; j < length_of_the_string; j++)
+		_putchar(str[j]);
+	_putchar('\n');
+}
