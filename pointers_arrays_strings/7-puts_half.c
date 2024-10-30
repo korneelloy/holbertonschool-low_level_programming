ccaddcbeb75dc4cc59c8b16cd8b01f1596e16748
korneel <korneel.loy@gmail.com>
2024-10-30 12:00:10 +0000

    C pointeurs ex 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		length_of_the_string++;
 	if (length_of_the_string % 2 == 1)
-		n = (length_of_the_string - 1) / 2;
+		n = (length_of_the_string + 1) / 2;
 	else
 		n = length_of_the_string / 2;
 	for (j = n; j < length_of_the_string; j++)
