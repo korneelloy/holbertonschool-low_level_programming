8d6bebfbf0bece0fb581a69542fb9d007c0fe6ea
korneel <korneel.loy@gmail.com>
2024-10-31 12:29:01 +0000

    C more pointers ex 4 en cours

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,12 @@
+/**
+ * reverse_array - reverses content of an array of integers
+ * @a: pointer to an array of integers
+ * @n: number of elements in the array
+ *
+ * Return: no return
+ */
+
+void reverse_array(int *a, int n)
+{
+	int provisoir[n];
+}
