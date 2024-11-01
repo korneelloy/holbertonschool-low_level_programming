85ac213dd3b017fd95418ac533e1821806181fcd
korneel <korneel.loy@gmail.com>
2024-11-01 09:09:45 +0000

    C more pointers ex 4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * reverse_array - reverses content of an array of integers
  * @a: pointer to an array of integers
@@ -8,5 +9,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int provisoir[n];
+	int i, temp;
+
+	for (i = 0; i <= (n / 2) - 1; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
 }
