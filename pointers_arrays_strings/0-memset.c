30c184380a51e7ad692a66d6f3b33e0e75562787
korneel <korneel.loy@gmail.com>
2024-11-04 10:26:49 +0000

    Even more pointers ex 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+/** 
+ * _memset - fills n cases of memory with b, starting at s
+ * @s: pointer to the start character 
+ * @b: the caracter that need to be filled in
+ * @n: number of cases toi be filled in
+ *
+ * Return: same pointer 
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
