baae3e4520b141f0ce810c03f1eddc7c7c69f161
korneel <korneel.loy@gmail.com>
2024-11-04 15:39:00 +0000

    Even more pointers ex 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,39 +1,33 @@
+#include <stdio.h>
 /**
- * _strpbrk - returns first occurence of one of the cahrs in a list
+ * _strpbrk - returns first occurence of one of the chars in a list
  * @s: pointer to a string to be checked
  * @accept: pointer to a list of characters to be checked against
  *
- * Return: pointer to first occurence 
+ * Return: pointer to first occurence
  */
 
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
-	char zero = '\0';
-	char *ptr = &zero;
+	char **ptr = &s;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (accept[0] == '\0')
 		{
-			return (ptr);
+			return (0);
 		}
 		else
 		{
 			for (j = 0; accept[j] != '\0'; j++)
 			{
-				if (s[i] != accept[j])
-				{
-					if (accept[j + 1] == '\0')
-						return (i);
-					continue;
-				}
 				if (s[i] == accept[j])
 				{
-					return (&s[i]);
+					return (*ptr + i);
 				}
 			}
 		}
 	}
-	return (i);
+	return (0);
 }
