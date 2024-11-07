7306f0afe8d7dfc4ca410e5c8d624aa2d1423e01
korneel <korneel.loy@gmail.com>
2024-11-07 15:11:52 +0000

    ex 6 en cours

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * is_prime_number - checks if a given nb is a prime nummber
  * @n: given number
@@ -7,22 +8,19 @@
 
 int is_prime(int n, int x)
 {
-	if ((n / x) % 2 == 0)
-		return (1);
-	return(is_prime(n, x-1));
-	return (0);
+	if (n % x == 0)
+		return (0);
+	if (x * x > n)
+	{
+		return(1);
+	}
+	return(is_prime(n, x + 1));
 }
 
 int is_prime_number(int n)
 {
-	int x;
 
-	if (n < 1)
+	if (n < 2)
 		return (0);
-	if (n == 0)
-		return (1);
-	x = is_prime(n, n - 1);
-	if (x == 0)
-		return (0);
-	return (1);
+	return (is_prime(n, 2));
 }
--- recursion/6-main.c
+++ recursion/6-main.c
@@ -12,10 +12,8 @@ int main(void)
 
     r = is_prime_number(1);
     printf("%d\n", r);
-   
     r = is_prime_number(1024);
     printf("%d\n", r);
-    
     r = is_prime_number(16);
     printf("%d\n", r);
     r = is_prime_number(17);
@@ -24,12 +22,9 @@ int main(void)
     printf("%d\n", r);
     r = is_prime_number(-1);
     printf("%d\n", r);
-   
-    
     r = is_prime_number(113);
     printf("%d\n", r);
     r = is_prime_number(7919);
     printf("%d\n", r);
-
     return (0);
 }
