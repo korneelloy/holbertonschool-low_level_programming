22808ef76980cfc1e3923cda74e6355de038f72d
korneel <korneel.loy@gmail.com>
2024-11-08 06:19:03 +0000

    argc argv ex 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function entry point
+ * @argc: number of arguments
+ * @argv: list of arguments
+ *
+ * Return: 0 is no error
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
