a49408fa477fb8eced7aa31c6e3f32eaaaaf14bc
korneel <korneel.loy@gmail.com>
2024-11-11 13:47:59 +0100

    malloc ex 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+/**
+ * _strdup - copies a string and returns new pointer
+ * @str: string
+ *
+ * Return: pointer to sting
+ */
+
+char *_strdup(char *str)
+{
+	int i, lenght = 0;
+	char *newstr = NULL;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '0'; i++)
+		lenght++;
+
+	newstr = malloc(lenght * sizeof(char));
+
+	if (newstr == NULL)
+		return (NULL);
+
+	for (i = 0; i <= lenght; i++)
+		newstr[i] = str[i];
+
+	return (newstr);
+}
