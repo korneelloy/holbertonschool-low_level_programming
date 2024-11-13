9826d443ec25011fcc0864ad91c8c19841018aaa
korneel <korneel.loy@gmail.com>
2024-11-13 12:40:16 +0100

    struct ex 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initlialize a new dog
+ * @d: pointer to new dog
+ * @name: pointer to name
+ * @age: age
+ * @owner: pointer to owner
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
