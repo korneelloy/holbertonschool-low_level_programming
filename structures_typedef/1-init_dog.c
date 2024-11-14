8aad0d209dc84c16a95e7d0ed66472669d59530d
korneel <korneel.loy@gmail.com>
2024-11-14 12:28:28 +0100

    strcut ex 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -14,22 +14,6 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	/**
-	*d->name = malloc(sizeof(name));
-	if (d->name == NULL)
-		return (NULL);
-	d->owner = malloc(sizeof(owner));
-	if (d->owner == NULL)
-		return (NULL);
-	*/
-
-	struct dog **ptr = NULL;
-
-	ptr = malloc(3 * sizeof(struct dog *));
-	ptr[0] = malloc(sizeof(name));
-	ptr[1] = malloc(sizeof(age));
-	ptr[2] = malloc(sizeof(owner));
-
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
