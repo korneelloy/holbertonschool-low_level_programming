33847711794adb5d6da5f5e3efa632000ad78d61
korneel <korneel.loy@gmail.com>
2024-11-15 14:24:06 +0100

    struct ex 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -28,20 +28,22 @@ int size_of_string(char *s)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr = NULL;
 	int i = 0, size_name = 0, size_owner = 0;
+	dog_t *ptr = malloc(sizeof(dog_t));
+
+	if (ptr == NULL)
+		return (NULL);
 
 	size_name = size_of_string(name);
 	size_owner = size_of_string(owner);
-	ptr = malloc(sizeof(dog_t));
-	if (ptr == NULL)
-		return (NULL);
+
 	ptr->name = malloc(size_name);
 	if (ptr->name == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
 	ptr->owner = malloc(size_owner);
 	if (ptr->owner == NULL)
 	{
@@ -49,10 +51,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(ptr);
 		return (NULL);
 	}
+
 	ptr->age = age;
-	for (i = 0; i <= size_name; i++)
+
+	for (i = 0; i < size_name; i++)
 		ptr->name[i] = name[i];
-	for (i = 0; i <= size_owner; i++)
+	ptr->name[i] = '\0';
+
+	for (i = 0; i < size_owner; i++)
 		ptr->owner[i] = owner[i];
+	ptr->owner[i] = '\0';
+
 	return (ptr);
 }
