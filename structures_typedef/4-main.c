26ff9a2b768fca9c359cea0561b2098b9e9ee5ca
korneel <korneel.loy@gmail.com>
2024-11-15 14:16:05 +0100

    struct ex 4 sizeof

--- structures_typedef/4-main.c
+++ structures_typedef/4-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h> 
 
 /**
  * main - check the code
@@ -8,9 +9,13 @@
  */
 int main(void)
 {
-    dog_t *my_dog;
+    dog_t *anotherdog;
 
-    my_dog = new_dog("Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+    anotherdog = new_dog("Ghost", 4.75, "Jon ccc");
+    printf("My name is %s, and I am %.1f :) - %s!\n", anotherdog->owner, anotherdog->age, anotherdog->name);
+    free(anotherdog->owner);
+    free(anotherdog->name);
+    free(anotherdog);
+
     return (0);
 }
--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,22 @@
 #include "dog.h"
 
 /**
+ * size_of_string - compteur nombre de char dans tsring + 1 pour null byte
+ * @s: string
+ *
+ * Return: size
+ */
+
+int size_of_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i++);
+}
+
+/**
  * new_dog - creating a new dog
  * @name: nom
  * @age: age
@@ -13,35 +29,30 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr = NULL;
-	long unsigned int i;
+	int i = 0, size_name = 0, size_owner = 0;
 
+	size_name = size_of_string(name);
+	size_owner = size_of_string(owner);
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return (NULL);
-	ptr->name = malloc(sizeof(name));
+	ptr->name = malloc(size_name);
 	if (ptr->name == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	ptr->owner = malloc(sizeof(owner));
+	ptr->owner = malloc(size_owner);
 	if (ptr->owner == NULL)
 	{
 		free(ptr->name);
 		free(ptr);
 		return (NULL);
 	}
-
 	ptr->age = age;
-
-	for (i = 0; i < sizeof(name); i++)
-	{
+	for (i = 0; i <= size_name; i++)
 		ptr->name[i] = name[i];
-	}
-	for (i = 0; i < sizeof(owner); i++)
-	{
+	for (i = 0; i <= size_owner; i++)
 		ptr->owner[i] = owner[i];
-	}
-
 	return (ptr);
 }
