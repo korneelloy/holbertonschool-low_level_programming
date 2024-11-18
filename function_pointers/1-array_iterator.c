14a66e5f76205cd2128c4bc4b594918db8ece4d6
korneel <korneel.loy@gmail.com>
2024-11-18 11:17:34 +0100

    function pointers ex 1 minor betty error

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i;
+	unsigned long int i;
 
 	if (array == NULL || size < 1 || action == NULL)
 		return;
