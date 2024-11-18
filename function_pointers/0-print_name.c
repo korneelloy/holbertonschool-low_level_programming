a5a3a6ced64d967cf021f2fd21a0add78493519c
korneel <korneel.loy@gmail.com>
2024-11-18 10:50:45 +0100

    function pointers ex 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name: prints given name according to function pointer
+ * @name: (pointer to string of) name
+ * @f: pointer to function type with char ptr as argument and no return
+ * 
+ * Return: no return
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,1 @@
+exercices function pointers
