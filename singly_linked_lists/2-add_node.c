b7dad5768ea452bbed7eb6e3ce606cf52f677471
korneel <korneel.loy@gmail.com>
2024-12-03 15:54:37 +0100

    singly linked lists ex 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,14 +12,29 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node = NULL;
+	char *string = strdup(str);
+	int i = 0, length = 0;
 
+	for (i = 0; string[i]; i++)
+		continue;
+	length = i;
+
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
-	new_node->len = strlen(new_node->str);
+	new_node->str = malloc(length * sizeof(char) + 1);
+	if (new_node->str == NULL)
+		return (NULL);
+	strcpy(new_node->str, string);
+
+	new_node->len = length;
+
+	free(string);
+
 	new_node->next = *head;
+
 	*head = new_node;
 
 	if (head == NULL)
