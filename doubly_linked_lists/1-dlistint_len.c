83cf84c56b01ea8fe5420be603da00b744a8cfff
korneel <korneel.loy@gmail.com>
2024-12-04 16:47:11 +0100

    doubly linked lists ex 1

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,18 @@
 #include "lists.h"
 
 /**
- * print_dlistint - print all the elements of a list
+ * dlistint_len - returns the number of elements in a list
  * @h: pointer to the list
  *
  * Return: the number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	dlistint_t *current = NULL;
 	int i = 0;
 
 	for (current = (dlistint_t *)h; current != NULL; current = current->next)
-	{
-		printf("%d\n", current->n);
 		i++;
-	}
 	return (i);
 }
