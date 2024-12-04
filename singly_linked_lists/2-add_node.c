7b377c800f5c5cd9b36db298089ad923cf0c988d
korneel <korneel.loy@gmail.com>
2024-12-04 09:53:04 +0100

    singly linked lists ex 2 refactoring strdup use

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "lists.h"
 #include <string.h>
+#include "lists.h"
 
 /**
  * add_node - adds node to beginning of a list
@@ -13,40 +13,27 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = NULL;
-	char *string = strdup(str);
-	int i = 0, length = 0;
-
-	if (string == NULL)
-		return (NULL);
-
-	for (i = 0; string[i]; i++)
-		continue;
-	length = i;
+	int length = 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
-		free(string);
 		return (NULL);
-	}
-	new_node->str = malloc((length + 1) * sizeof(char));
+
+	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
-		free(string);
 		free(new_node);
 		return (NULL);
 	}
-	strcpy(new_node->str, string);
+
+	for (length = 0; new_node->str[length]; length++)
+		continue;
 
 	new_node->len = length;
-
-	free(string);
 
 	new_node->next = *head;
 
 	*head = new_node;
-
-	printf("%p\n", (void *)*head);
 
 	return (*head);
 }
