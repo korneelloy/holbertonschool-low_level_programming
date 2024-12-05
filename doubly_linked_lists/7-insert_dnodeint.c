5c55f35888802050cfb76c5e5a38a7ef9bc0bcdd
korneel <korneel.loy@gmail.com>
2024-12-05 09:50:07 +0100

    doubly linked lists ex 7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+
+/**
+ * insert_dnodeint_at_index - insert a number in a list
+ * @h: double pointer to list
+ * @idx: index where to add
+ * @n: nimber to be addes
+ *
+ * Return: pointer to new node or NULL if faile
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current = NULL;
+	unsigned int i = 0;
+	dlistint_t *new_node = NULL;
+
+	if (*h == NULL)
+	{
+		if (idx == 0)
+			return (add_dnodeint(h, n));
+		else
+			return (NULL);
+	}
+
+	for (current = *h; current != NULL; current = current->next)
+	{
+		if (i == idx)
+			break;
+		i++;
+	}
+
+	if (i == 0)
+		return (add_dnodeint(h, n));
+
+	else if (current->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	else if (i < idx)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = current;
+	new_node->prev = current->prev;
+
+	if (current->prev != NULL)
+		current->prev->next = new_node;
+	current->prev = new_node;
+	return (new_node);
+}
---
+++ doubly_linked_lists/7-main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	dlistint_t *head;
+
+	head = NULL;
+	
+	printf("-----------------\n");
+	insert_dnodeint_at_index(&head, 0, 4096);
+	print_dlistint(head);
+	free_dlistint(head);
+	head = NULL;
+	return (EXIT_SUCCESS);
+}
