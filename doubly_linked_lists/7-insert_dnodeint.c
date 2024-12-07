3948b7f8937ae1edcf7c65ed8bdfd4d01781d07f
korneel <korneel.loy@gmail.com>
2024-12-07 12:53:41 +0100

    doubly luunked lists ex 7 node sequence and end of list conditions

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current = NULL, *new_node = NULL;
-	unsigned int i = 0;
+	unsigned int i = 0, lenght = 0;
 	/**  if list empty, add, but only if index given is 0 */
 	if (*h == NULL)
 	{
@@ -23,30 +23,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	for (current = *h; current != NULL; current = current->next)
+		lenght++;
+
+	for (current = *h; current != NULL; current = current->next)
 	{
 		if (i == idx)
 			break;
 		i++;
 	}
+	/** if index en dehors de la liste */
+	if (i < idx)
+		return (NULL);
 	/** if index 0, call add at beginning*/
-	if (i == 0)
+	else if (i == 0)
 		return (add_dnodeint(h, n));
 	/** if index at last node, call add at end */
-	else if (current && current->next == NULL)
+	else if (idx == lenght)
 		return (add_dnodeint_end(h, n));
-	/** else add in middle if index is not greater than lenght list */
-	else if (i < idx)
-		return (NULL);
-
+	/** else add in middle */
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
+	new_node->n = n;
 	new_node->next = current;
 	new_node->prev = current->prev;
-
-	if (current->prev != NULL)
-		current->prev->next = new_node;
+	current->prev->next = new_node;
 	current->prev = new_node;
+
 	return (new_node);
 }
