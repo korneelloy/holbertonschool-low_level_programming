a050823952ab4ca5e48764f78fa5bd2f41aed615
korneel <korneel.loy@gmail.com>
2024-12-07 11:42:57 +0100

    file IO ex 1 handling NULL case for text content

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,12 +20,15 @@ int create_file(const char *filename, char *text_content)
 		if (file == -1)
 			return (-1);
 
-	size_written = write(file, text_content, strlen(text_content));
-		if (size_written == -1)
-		{
-			close(file);
-			return (-1);
-		}
+	if (text_content != NULL)
+	{
+		size_written = write(file, text_content, strlen(text_content));
+			if (size_written == -1)
+			{
+				close(file);
+				return (-1);
+			}
+	}
 	closed = close(file);
 		if (closed == -1)
 			return (-1);
